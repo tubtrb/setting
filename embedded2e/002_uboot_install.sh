ca99ab0aecaf6dd2d4d6f2aa8a16ff345de6ce3f
tubtrb <ggssggwork@gmail.com>
2023-06-04 22:09:56 +0900

    delete make command

--- embedded2e/002_uboot_install.sh
+++ embedded2e/002_uboot_install.sh
@@ -7,5 +7,3 @@
 git clone git@github.com:tubtrb/u-boot.git
 cd u-boot
 
-make CROSS_COMPILE=arm-cortex_a8-linux-gnueabihf- am335x_boneblack_defconfig
-make CROSS_COMPILE=arm-cortex_a8-linux-gnueabihf- ARCH=arm CFLAGS="-march=armv7 -mtune=cortex-a8"
